81a23a5dedabdedcdfbe65a5f66373f353fcea1f
b2 <brendan@mobilewebsmart.com>
2021-04-26 07:03:46 -0600

    core-v1/04-effects/lecture

--- courses/core-v1/04-effects/lecture/ProductsSidebar.tsx
+++ courses/core-v1/04-effects/lecture/ProductsSidebar.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 import ProductFilters from 'YesterTech/ProductFilters'
 
 function ProductsSidebar() {
+	const query = '(min-width: 800px)';
+	const [isWide, setIsWide] = useState(window.matchMedia(query).matches);
+
+	useEffect(() => {
+		const media = window.matchMedia(query);
+		const listener = () => setIsWide(media.matches);
+		media.addEventListener('change', listener);
+		return () => media.removeEventListener('change', listener);
+	}, [isWide]);
+
+	if(!isWide) return <></>;
+
   return (
     <aside>
       <ProductFilters />
